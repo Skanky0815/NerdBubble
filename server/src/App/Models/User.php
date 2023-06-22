19b00c57da86ae7daa511f88f9bd9762b2f779dd
Skanky0815 <rico-schulz@web.de>
2023-06-22 23:49:43 +0200

    clean up User.php for sonar

--- server/src/App/Models/User.php
+++ server/src/App/Models/User.php
@@ -4,29 +4,31 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminat e\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken;
 
 /**
  * App\Models\User.
  *
- * @property int                                                                                                           $id
- * @property string                                                                                                        $name
- * @property string                                                                                                        $email
- * @property null|\Illuminate\Support\Carbon                                                                               $email_verified_at
- * @property string                                                                                                        $password
- * @property null|string                                                                                                   $remember_token
- * @property null|\Illuminate\Support\Carbon                                                                               $created_at
- * @property null|\Illuminate\Support\Carbon                                                                               $updated_at
- * @property \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
- * @property null|int                                                                                                      $notifications_count
- * @property \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken>                           $tokens
- * @property null|int                                                                                                      $tokens_count
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property null|\Illuminate\Support\Carbon $email_verified_at
+ * @property string $password
+ * @property null|string $remember_token
+ * @property null|\Illuminate\Support\Carbon $created_at
+ * @property null|\Illuminate\Support\Carbon $updated_at
+ * @property DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property null|int $notifications_count
+ * @property \Illuminate\Database\Eloquent\Collection<int, PersonalAccessToken> $tokens
+ * @property null|int $tokens_count
  *
  * @method static \Database\Factories\UserFactory            factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
@@ -41,19 +43,19 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  *
- * @property null|string                                                                                                   $two_factor_secret
- * @property null|string                                                                                                   $two_factor_recovery_codes
- * @property \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
- * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Product>                                            $products
- * @property null|int                                                                                                      $products_count
- * @property \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken>                           $tokens
+ * @property null|string $two_factor_secret
+ * @property null|string $two_factor_recovery_codes
+ * @property DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Product> $products
+ * @property null|int $products_count
+ * @property \Illuminate\Database\Eloquent\Collection<int, PersonalAccessToken> $tokens
  *
  * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
  *
- * @property \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
- * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Product>                                            $products
- * @property \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken>                           $tokens
+ * @property DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Product> $products
+ * @property \Illuminate\Database\Eloquent\Collection<int, PersonalAccessToken> $tokens
  *
  * @mixin \Eloquent
  */
