15ca364bbcb029bcd97fa3800e95468c9e5eeee4
Skanky0815 <rico-schulz@web.de>
2024-08-15 19:50:19 +0200

    feat: enhance footer navigation for mobile

--- app/src/application/components/bottom-navigation/BottomNavigationItem.tsx
+++ app/src/application/components/bottom-navigation/BottomNavigationItem.tsx
@@ -9,7 +9,7 @@ type BottomNavigationItemProps = {
 
 export default function BottomNavigationItem({to, color, children}: PropsWithChildren<BottomNavigationItemProps>) {
     const classes = ({ isActive, isPending }: {isActive: boolean, isPending: boolean}) =>
-        isPending || isActive ? `p-2 text-sm text-white bg-gray-500` : `p-2 text-sm text-gray-500 group-hover:text-${color}-800`;
+        isPending || isActive ? `p-2 border-b-2 border-${color}-500 text-sm text-white bg-gray-500` : `p-2 pb-4 text-sm text-gray-500 group-hover:text-${color}-800`;
 
     return (
         <NavLink to={to} className={classes}>
